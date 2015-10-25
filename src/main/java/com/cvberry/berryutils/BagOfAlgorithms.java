f931dba07b543960d3bbd937c1fbd7dcb75ea415
Currell Berry <currellberry@gmail.com>
2015-10-25 10:12:00 -0400

    working linear solver for underdetermined and well-determined systems.

--- src/main/java/com/cvberry/berryutils/BagOfAlgorithms.java
+++ src/main/java/com/cvberry/berryutils/BagOfAlgorithms.java
@@ -116,6 +116,19 @@ public class BagOfAlgorithms {
         return out;
     }
 
+    public static double[] mat_vec_mult(double[][] mat, double[] vec) {
+        int[] shp = mat_shape(mat);
+        assert(shp[1] == vec.length);
+        double[] out = new double[shp[0]];
+        for (int m1row = 0; m1row < shp[0]; m1row++) {
+            double value = 0;
+            for (int index = 0; index < shp[1]; index++) {
+                value += mat[m1row][index]*vec[index];
+            }
+            out[m1row] = value;
+        }
+        return out;
+    }
 
     public static boolean mat_equals(double[][] m1, double[][] m2) {
         return Arrays.deepEquals(m1,m2); //CB I think this should work.
@@ -124,7 +137,6 @@ public class BagOfAlgorithms {
     public static boolean mat_equals(boolean[][] m1, boolean[][] m2) {
         return Arrays.deepEquals(m1,m2); //CB I think this should work.
     }
-
 
     public static boolean mat_equals(double[][] m1, double[][] m2, double tolerance) {
         int[] shp1 = mat_shape(m1);
@@ -137,6 +149,23 @@ public class BagOfAlgorithms {
                     //System.out.format("b: %f, a: %f, tolerance: %f\n",m2[m][n],m1[m][n],tolerance);
                     return false;
                 }
+                if(Double.isNaN(m2[m][n]) || Double.isNaN(m1[m][n])) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean vec_equals(double[] v1, double[] v2, double tolerance) {
+        if (v1.length != v2.length) {
+            return false;
+        }
+
+        for (int i =0; i < v1.length; i++) {
+            if (Math.abs(v1[i]-v2[i]) > tolerance) {
+                return false;
             }
         }
 
@@ -225,6 +254,22 @@ public class BagOfAlgorithms {
         }
         return mat;
     }
+
+    public static double[] vec_print(double[] vec) {
+        vec_print(vec,"");
+        return vec;
+    }
+
+
+    public static double[] vec_print(double[] vec, String message) {
+        System.out.println(message);
+        for (int m = 0; m < vec.length; m++) {
+            System.out.format("%f ", vec[m]);
+            System.out.println();
+        }
+        return vec;
+    }
+
 
     public static double vec_dot_product(double[] a, double[] e) {
         assert(a.length==e.length);
@@ -315,49 +360,190 @@ public class BagOfAlgorithms {
         return out;
     }
 
-    /** based on https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process#Numerical_stability
-     */
-    public static double[][][] qr_gschmidt(double[][] mat) {
+    public static double[][] mat_splice_out_column(double[][] mat, int col) {
         int[] shp = mat_shape(mat);
+        double[][] out = new double[shp[0]][shp[1]-1];
+        //copy over cols before col
+        for (int n = 0; n < col; n++) {
+            for (int m = 0; m < shp[0]; m++) {
+                out[m][n]=mat[m][n];
+            }
+        }
+        //copy over cols after col
+        for (int n = col+1; n < shp[1]; n++) {
+            for (int m = 0; m < shp[0]; m++) {
+                out[m][n-1]=mat[m][n];
+            }
+        }
+        return out;
+    }
 
-        double[][] Q = new double[shp[0]][shp[1]];
-        double[][] R = new double[shp[0]][shp[1]];
+    public static double[][] mat_splice_out_row(double[][] mat, int row) {
+        int[] shp = mat_shape(mat);
+        double[][] out = new double[shp[0]-1][shp[1]];
+        //copy over rows before row
+        for (int m = 0; m < row; m++) {
+            for (int n = 0; n < shp[1]; n++) {
+                out[m][n]=mat[m][n];
+            }
+        }
+        //copy over rows after row
+        for (int m = row+1; m < shp[0]; m++) {
+            for (int n = 0; n < shp[1]; n++) {
+                out[m-1][n]=mat[m][n];
+            }
+        }
+        return out;
+    }
 
-        double[] col0 = mat_grab_column(mat,0);
-        for (int n = 0; n < shp[1]; n++) {
+    public static double[][][] qr_gschmidt(double[][] mat) {
+        return qr_gschmidt(mat, 0.000001);
+    }
+
+    /** based on https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process#Numerical_stability
+        @param mat the matrix to decompose
+        @param singularThreshold the threshold for the norm of a vector under which to treat it as 0
+     */
+    public static double[][][] qr_gschmidt(double[][] mat, double singularThreshold) {
+        int[] shpOrig = mat_shape(mat);
+
+        double[][] Q = new double[shpOrig[0]][shpOrig[0]];
+        int[] shpQ = mat_shape(Q);
+        double[][] R = new double[shpOrig[0]][shpOrig[1]];
+
+        int rank = 0;
+        int qoffset = 0; //tracks how many columns back to track Q versus mat.  When a dependent column is found in mat,
+        //this number is incremented
+        for (int n = 0; n < shpOrig[1]; n++) {
+            int nq = n-qoffset;
             //first, get original column
             double[] ocol = mat_grab_column(mat,n);
             //next, calculate evec: ocol less the projections of ocol onto each of the previous Q vectors
             double[] uk_est = Arrays.copyOf(ocol, ocol.length);
-            for (int n1 = 0; n1 < n; n1++) {
-                double[] currentQCol = mat_grab_column(Q,n1);
-                double Qcol_ocol_dp = vec_dot_product(ocol,currentQCol);
-                R[n1][n] = Qcol_ocol_dp;
 
-                //below is standard gram-schmidt
-                //uk_est = vec_sub(uk_est, vec_scalar_mult(Qcol_ocol_dp,currentQCol));
-                //below is modified gram-schmidt.  better stability supposedly.
-                uk_est = vec_sub(uk_est,  vec_proj_a_onto_e(uk_est,currentQCol));
-            }
+            if (nq < shpQ[1]) {
+                for (int n1 = 0; n1 < nq; n1++) {
+                    double[] currentQCol = mat_grab_column(Q,n1);
+                    double Qcol_ocol_dp = vec_dot_product(ocol,currentQCol);
+                    R[n1][n] = Qcol_ocol_dp;
 
-            double uk_est_norm = vec_norm(uk_est);
-            R[n][n] = uk_est_norm;
+                    //below is standard gram-schmidt
+                    //uk_est = vec_sub(uk_est, vec_scalar_mult(Qcol_ocol_dp,currentQCol));
+                    //below is modified gram-schmidt.  better stability supposedly.
+                    uk_est = vec_sub(uk_est, vec_proj_a_onto_e(uk_est,currentQCol));
+                }
 
-            double[] evec = vec_scalar_mult(1.0/uk_est_norm, uk_est);
-            for (int m = 0; m < shp[0]; m++) {
-                Q[m][n] = evec[m];
+                double uk_est_norm = vec_norm(uk_est);
+                if (uk_est_norm < singularThreshold) {
+                    //then we have a 0 vector, splice out from Q and R
+                    //Q = mat_splice_out_column(Q,nq);
+                    //R = mat_splice_out_row(R,nq);
+                    //shpQ[1] = shpQ[1]-1;
+                    qoffset+=1;
+                    continue;
+                }
+                R[nq][n] = uk_est_norm;
+
+                double[] evec = vec_scalar_mult(1.0/uk_est_norm, uk_est);
+                rank +=1;
+                for (int m = 0; m < shpOrig[0]; m++) {
+                    Q[m][nq] = evec[m];
+                }
+            } else {
+                for (int n1 = 0; n1 < shpQ[1]; n1++) {
+                    double[] currentQCol = mat_grab_column(Q,n1);
+                    double Qcol_ocol_dp = vec_dot_product(ocol,currentQCol);
+                    R[n1][n] = Qcol_ocol_dp;
+                }
             }
         }
-        // mat_print(mat, "input");
-        // mat_print(Q, "Q");
-        // mat_print(R, "R");
+        //now remove the entries which didn't get populated because of non-full rank
+        for (int n = rank; n < shpQ[0]; n++) {
+           Q = mat_splice_out_column(Q, rank);
+           R = mat_splice_out_row(R, rank);
+        }
 
-        double[][][] out = new double[2][shp[0]][shp[1]];
+        double[][][] out = new double[2][shpOrig[0]][shpOrig[1]];
         out[0] = Q;
         out[1] = R;
         return out;
     }
 
+    /**
+     * solves for x in R*x=y, where R is an upper triangular matrix.
+     * can handle under-determined values of R (ie mxn, n > m)
+     *
+     * R: m <= n
+     */
+    public static double[] mat_backSubstUpperTriangularMatrix(double[][] R, double[] y) {
+        int[] shp = mat_shape(R);
+        double[][] tR = new double[shp[0]][shp[0]];
+        for (int m = 0; m < shp[0]; m++) {
+            for (int n = 0; n < shp[0]; n++) {
+                tR[m][n] = R[m][n];
+            }
+        }
+
+        int[] tShp = mat_shape(tR);
+
+        double[] out = new double[shp[1]];
+
+        //now, do back substitution
+        for (int level = y.length-1; level >= 0; level--) {
+            double lans = y[level];
+            double subtractor = 0;
+            for (int n = level+1; n<tShp[1]; n++) {
+                subtractor += tR[level][n]*out[n];
+            }
+            out[level]=(lans-subtractor)/tR[level][level];
+        }
+
+        return out;
+    }
+
+    //CB TODO fix: get the system solver working.
+    /** by default throw an error if the system is under or over determined */
+    public static double[] mat_solveSystem(double[][] A, double[] x, double[] b) {
+        return mat_solveSystem(A,x,b,false);
+    }
+
+    public static double[] mat_solveSystem(double[][] A, double[] x, double[] b, boolean lstSquaresSolution) {
+        return x;
+    }
+
+    //next up -- add least squares solver support.
+    //solves QRx=b, where QR is composed of independent vectors.
+    //uses QR, because that is what I have coded.
+    //will return a solution to underdetermined systems.
+    public static double[] _mat_solveSystemNoLstSq(double[][] Q, double[][] R, double[] b) {
+        int rank = mat_shape(Q)[1];
+        int aheight = mat_shape(Q)[0]; //number of independent equations
+        int awidth = mat_shape(R)[1];
+        int num_unknowns = awidth;
+        double[] out = new double[awidth];//but this gets replaced below
+
+        if(rank < num_unknowns) {
+            //fewer independent equations than unknowns
+            //underdetermined, we use QR to give us one (of infinite) valid answers.
+            double[] Qtb = mat_vec_mult(mat_transpose(Q),b);
+            out = mat_backSubstUpperTriangularMatrix(R,Qtb);
+        } else if (rank == num_unknowns) {
+            if(aheight == num_unknowns) {
+                //then we have a full rank system with unique solution.
+                double[] Qtb = mat_vec_mult(mat_transpose(Q),b);
+                out = mat_backSubstUpperTriangularMatrix(R,Qtb);
+            } else if (aheight > num_unknowns) {
+                //then we have an OVERdetermined system, more equations than unknowns.
+            } else {
+                //not possible. aheight cannot be greater than rank.
+                throw new RuntimeException("it is impossible to get here...");
+            }
+        } else if (rank > num_unknowns) {
+            //I believe this is impossible.
+            throw new RuntimeException("it is impossible to get here...");
+        }
+        return out;
+    }
 
     public static double[][] mat_rand(int height, int width) {
         Random random = new Random();
@@ -376,4 +562,42 @@ public class BagOfAlgorithms {
         return out;
     }
 
+    public static double[] vec_rand(int length) {
+        Random random = new Random();
+        long seed = random.nextLong();
+        return vec_rand(length,seed);
+    }
+
+    public static double[] vec_rand(int length, long seed) {
+        Random random = new Random(seed);
+        double[] out = new double[length];
+        for (int m = 0; m < length; m++) {
+            out[m] = random.nextDouble();
+        }
+        return out;
+    }
+
+    public static double[][] mat_get_sub_matrix(double[][] mat, int m_start, int m_end, int n_start, int n_end) {
+       double[][] out = new double[m_end-m_start][n_end-n_start];
+       for (int m = m_start; m < m_end; m++) {
+           for (int n = n_start; n < n_end; n++) {
+              out[m-m_start][n-n_start] = mat[m][n];
+           }
+       }
+        return out;
+    }
+
 }
+
+//linearSolveSquare seed: 2649078803596194174
+// A = np.matrix(
+//           [[0.489605,0.173206,0.046936],
+//            [0.361378,0.878040,0.088176],
+//            [0.545823,0.193877,0.908471]])
+
+// b = np.array([0.170023,0.439159,0.602410])
+
+    //solution =
+    //-1.434132 
+    //1.018217 
+    //1.053430 
--- src/test/java/com/cvberry/BagTest.java
+++ src/test/java/com/cvberry/BagTest.java
@@ -201,6 +201,83 @@ public class BagTest
         }
     }
 
+    @Test
+    public void test_qr_underdetermined() {
+        double[][] mat1 = new double[][]{{1, 2, 0},
+                                        {1, 3, 0},
+                                        {0, 0, 0}};
+
+        double[][] mat2 = new double[][]{{1, 2, 3},
+                                         {1, 3, 5},
+                                         {0, 0, 0}};
+
+        double[][] mat3 = new double[][]{{1, 0, 2},
+                                         {1, 0, 3},
+                                         {0, 0, 0}};
+
+
+        testQRFactorization(mat1);
+        testQRFactorization(mat2);
+        testQRFactorization(mat3);
+    }
+
+    @Test
+    public void test_qr_skinny() {
+        double[][] mat1 = new double[][]{{1, 2},
+                                        {1, 3},
+                                        {0, 0}};
+
+        double[][] mat2 = new double[][]{{0, 1},
+                                         {0, 0},
+                                         {0, 1}};
+
+        double[][] mat3 = new double[][]{{1, 1, 1},
+                                         {1, 1, 0},
+                                         {1, 0, 2},
+                                         {1, 0, 0}
+        };
+
+
+        testQRFactorization(mat1);
+        testQRFactorization(mat2);
+        testQRFactorization(mat3);
+    }
+
+    @Test
+    public void test_qr_wide() {
+        double[][] mat1 = new double[][]{{1,2,1},
+                                        {1,3,5}};
+
+        double[][] mat2 = new double[][]{{0, 1,0},
+                                         {0, 0,1}};
+
+        double[][] mat3 = mat_transpose(new double[][]{{1, 1, 1},
+                                         {1, 1, 0},
+                                         {1, 0, 2},
+                                         {1, 0, 0}
+        });
+
+
+        testQRFactorization(mat1);
+        testQRFactorization(mat2);
+        testQRFactorization(mat3);
+    }
+
+
+    public static void testQRFactorization(double[][] matrix) {
+        double[][][] factorization = qr_gschmidt(matrix);
+        double[][] Q = factorization[0];
+        double[][] R = factorization[1];
+        double[][] recomp = mat_mult(Q,R);
+        // mat_print(matrix, "input");
+        // mat_print(Q, "Q");
+        // mat_print(R, "R");
+        // mat_print(recomp, "recomp");
+        // System.out.println();
+
+        assertTrue(mat_equals(recomp,matrix,0.0001));
+    }
+
 
     @Test
     public void test_mult() {
@@ -220,7 +297,103 @@ public class BagTest
 
         assertTrue(mat_equals(mat_mult(mat,mat),matsquared,0.0001));
         assertFalse(mat_equals(mat_mult(omat,mat),matsquared,0.0001));
+    }
 
+    @Test
+    public void test_mult2() {
+        double[][] mat = new double[][]{{2, 1, 12},
+                                        {3, 1, -5}};
+
+        double[][] tresult = new double[][]
+            {{149, -53},
+             {-53, 35}};
+
+        assertTrue(mat_equals(mat_mult(mat,mat_transpose(mat)),tresult,0.0001));
+    }
+
+    @Test
+    public void test_qrTargeted() {
+        double[][] mat = new double[][]{{2, 1, 2},
+                                        {3, 1, 2},
+                                        {-2, 5, 10}};
+
+        testQRFactorization(mat);
+    }
+
+    @Test
+    public void test_backSubstitution() {
+        double[][] R1 = new double[][]
+            {{1, 5, 3},
+             {0, 2, 4},
+             {0, 0, 9}};
+
+        double[] y1 = new double[]{1,4,9};
+        double[] ans1 = new double[]{-2,0,1};
+
+        double[] out = mat_backSubstUpperTriangularMatrix(R1,y1);
+        //vec_print(out,"result");
+
+        assertTrue(vec_equals(out,ans1,0.0000001));
+
+        double[][] R2 = new double[][]{
+            {0.817453, 0.621355, 0.673689},
+            {0.000000, 0.672652, -0.233282},
+            {0.000000, 0.000000, 0.571856}};
+
+    }
+
+    @Test
+    public void test_linearSolveSquare() {
+        Random random = new Random();
+        long seed = random.nextLong();
+        System.out.format("linearSolveSquare -- using seed: %d\n", seed);
+        for (int i = 0; i < 25; i++) {
+            double[][] mat = mat_rand(8,8);
+            double[] vec = vec_rand(mat_shape(mat)[0]);
+            checkLinSolveSolution(mat,vec);
+        }
+    }
+
+    @Test
+    public void test_linearSolveWide() {
+        Random random = new Random();
+        long seed = random.nextLong();  //3580959054035785765l;
+        System.out.format("linearSolveWide -- using seed: %d\n", seed);
+        for (int i = 0; i < 25; i++) {
+            int nWidth = random.nextInt(5)+5;
+            double[][] mat = mat_rand(4,nWidth);
+            double[] vec = vec_rand(mat_shape(mat)[0]);
+            checkLinSolveSolution(mat,vec);
+        }
+    }
+
+
+    //for underdetermined or rull-rank systems.
+    public void checkLinSolveSolution(double[][] A, double[] b) {
+        //mat_print(A,"A");
+        double[][][] qr = qr_gschmidt(A);
+        double[][] Q = qr[0];
+        double[][] R = qr[1];
+        double[] solution = _mat_solveSystemNoLstSq(Q,R,b);
+        // mat_print(Q,"Q");
+        // mat_print(R,"R");
+        // vec_print(solution,"solution");
+        // vec_print(b,"b");
+        double[] reconstructed = mat_vec_mult(A,solution);
+        // vec_print(reconstructed,"reconstructed");
+        assertTrue(vec_equals(b,reconstructed,0.000001));
+    }
+
+    @Test
+    public void test_mat_vec_mult() {
+        double[] a = new double[]{1,2,3};
+        double[][] mat = new double[][]{{2, 1, 2},
+                                        {3, 1, 2},
+                                        {-2, 5, 10}};
+        double[] correct = new double[]{10,11,38};
+
+        double[] out = mat_vec_mult(mat,a);
+        assertTrue(vec_equals(correct,out,0.000001));
     }
 
 }
